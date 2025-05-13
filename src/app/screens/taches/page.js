d21feb4b45d98a40361518c187e48fd78f14d925
asaleydiori <asaleydiori@gmail.com>
2025-05-13 15:28:46 +0100

    orint the response2

--- src/app/screens/taches/page.js
+++ src/app/screens/taches/page.js
@@ -84,7 +84,7 @@ export default function Tasks() {
         method: "POST",
         body: JSON.stringify(newTask),
       });
-      console.log("response",response.message);
+      console.log("response",response);
       // console.log("response",response.);
       if (!response.ok) {
         throw new Error("erreur de réseau");
