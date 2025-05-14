c86d0333dfce533b39dc071eff84429ad59cd7be
asaleydiori <asaleydiori@gmail.com>
2025-05-14 10:52:19 +0100

    add echeance in the request

--- src/app/api/tasks/add/route.ts
+++ src/app/api/tasks/add/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     try {
         const body = await request.json();
-        const { titre, description, id_projet, start_date, end_date, precedence, asign_to } = body;
+        const { titre, description, id_projet, start_date, echeance,end_date, precedence, asign_to } = body;
 
         const client = await connectionPool.connect();
 
@@ -14,7 +14,7 @@ export async function POST(request: Request) {
                 (titre, description, start_date, echeance)
                 VALUES ($1, $2, $3, $4)
                 RETURNING *`,
-                [titre, description, id_projet, start_date, end_date]
+                [titre, description, start_date,echeance ]
             );
 
             const newTache = result.rows[0];
