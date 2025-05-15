af1830c31826ad1cd344ca2a35939241052ccc7c
asaleydiori <asaleydiori@gmail.com>
2025-05-15 16:32:48 +0100

    test de l'api send

--- src/app/screens/projets/page.js
+++ src/app/screens/projets/page.js
@@ -179,6 +179,24 @@ export default function Projects() {
     setIsSharing(true);
     let emailSent = false;
 
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: formData.email, // Change to your recipient
+      from: 'asaleydiori@gmail.com', // Change to your verified sender
+      subject: 'Invitation a un projet',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
     try {
       const emailResponse = await fetch('/api/send-email', {
         method: 'POST',
