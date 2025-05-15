d249edc7812adfa21de3299c619d7282b3b4a1e3
asaleydiori <asaleydiori@gmail.com>
2025-05-15 09:17:34 +0100

    add crypto package

--- src/app/screens/projets/page.js
+++ src/app/screens/projets/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { Toaster } from "@/components/ui/sonner";
+import crypto from 'crypto';
 import { toast } from "sonner";
 import {
   Table,
