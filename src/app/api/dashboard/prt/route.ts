6d24055e3316c7abc6f895cac74cc6eaee65b5af
asaleydiori <asaleydiori@gmail.com>
2025-05-20 09:09:39 +0100

    latest tasks and projects

--- src/app/api/dashboard/prt/route.ts
+++ src/app/api/dashboard/prt/route.ts
@@ -6,11 +6,14 @@ export async function GET() {
     try{
         const client = await connectionPool.connect();
         console.log("connected!")
-        const result = await client.query("WITH derniers_projets AS (SELECT id, title, description, start_date, end_date, state, created_at FROM projets ORDER BY created_at DESC LIMIT 3), dernieres_taches AS (SELECT id, titre, description, id_projet, start_date, state, created_at FROM taches ORDER BY created_at DESC LIMIT 3) SELECT 'projet' AS type, id, title AS nom, description, start_date, end_date, state, created_at FROM derniers_projets UNION ALL SELECT 'tache' AS type, id, titre AS nom, description, start_date, NULL AS end_date, state, created_at FROM dernieres_taches ORDER BY created_at DESC;");
-        const data = result.rows;
-        console.log("data",data);
+        const projects = await client.query("SELECT 'projet' AS type, id, title AS nom, description, start_date, end_date, state, created_at FROM projets ORDER BY created_at DESC LIMIT 3;");
+        const prodata = projects.rows;
+
+        const tasks = await client.query("SELECT 'projet' AS type, id, title AS nom, description, start_date, end_date, state, created_at FROM projets ORDER BY created_at DESC LIMIT 3;");
+        const taskdata = tasks.rows;
+        // console.log("data",data);
         client.release();
-        return NextResponse.json({data}, {status: 200});
+        return NextResponse.json({prodata,taskdata}, {status: 200});
     }catch(error){
         if (error instanceof Error) {
             return NextResponse.json({error: error.message}, {status: 500});
