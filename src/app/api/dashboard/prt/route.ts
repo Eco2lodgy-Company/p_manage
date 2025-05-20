61be49f5c4389793f2a91ff771fbf15d1efa5e07
asaleydiori <asaleydiori@gmail.com>
2025-05-20 08:52:15 +0100

    lkflkd

--- src/app/api/dashboard/prt/route.ts
+++ src/app/api/dashboard/prt/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
     try{
         const client = await connectionPool.connect();
         console.log("connected!")
-        const result = await client.query("WITH derniers_projets AS (SELECT id,title,description,start_date,end_date,state,created_at FROM projets ORDER BY created_at DESC LIMIT 3 ) dernieres_taches AS (SELECT id,titre,description,id_projet,start_date,state,created_at FROM taches ORDER BY created_at DESC LIMIT 3) SELECT 'projet' AS type,id,title AS nom,description,start_date,end_date,state,created_at FROM derniers_projets UNION ALL SELECT 'tache' AS type,id,titre AS nom,description,start_date,NULL AS end_date,state,created_at FROM dernieres_taches ORDER BY created_at DESC;" ");
+        const result = await client.query("WITH derniers_projets AS (SELECT id,title,description,start_date,end_date,state,created_at FROM projets ORDER BY created_at DESC LIMIT 3 ) dernieres_taches AS (SELECT id,titre,description,id_projet,start_date,state,created_at FROM taches ORDER BY created_at DESC LIMIT 3) SELECT 'projet' AS type,id,title AS nom,description,start_date,end_date,state,created_at FROM derniers_projets UNION ALL SELECT 'tache' AS type,id,titre AS nom,description,start_date,NULL AS end_date,state,created_at FROM dernieres_taches ORDER BY created_at DESC;");
         const data = result.rows;
         console.log("data",data);
         client.release();
